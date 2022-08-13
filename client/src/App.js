68f320197586f1ee2c7548e6e51e0e989e343fd0
rgiinther <r.giin95@gmail.com>
2022-08-12 19:07:58 -0500

    added Nav and projects

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 //import components
 import About from './components/About';
-
-
+import Nav from './components/Nav';
+import Projects from './components/Projects';
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
+        <Projects></Projects>
       </main>
     </div>
   );
---
+++ client/src/data.js
@@ -0,0 +1,20 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "Pet Pals",
+      subtitle: "MERN Stack",
+      description:
+        "This is a website to help make playdates for your pets",
+      image: "./project-1.gif",
+      link: "https://pet-pal-friend-finder.herokuapp.com",
+    },
+    {
+        title: "GeoCHAT",
+        subtitle: "Express, NodeJS, JavaScript, CSS" ,
+        description: "This is a website to connect with your neighbors",
+        image:"",
+        link: "https://geo-chat-neighborhoods.herokuapp.com/homepage",
+    },
+    
+]
